2abd3d850e921e3dd60509bf83b4a956118804e9
Fleuri Adon <fleuri.ad@gmail.com>
2015-04-17 20:12:16 +0200

    Create Face.h

---
+++ Camera-Pi/Face.h
@@ -0,0 +1,48 @@
+
+#ifndef FACE_H_
+#define FACE_H_
+
+#include <opencv/cv.h>
+
+using namespace cv;
+
+class Face
+{
+	
+protected:
+	Rect rect;
+	
+	bool eyesFound;
+	bool mouthFound;
+	bool noseFound;
+	
+	Rect leftEye;
+	Rect rightEye;
+	Rect mouth;
+	Rect nose;
+	
+public:
+	Face(Rect& faceRect);
+	virtual ~Face();
+	
+	Rect& getRect();
+
+	bool hasEyes();
+	bool hasMouth();
+	bool hasNose();
+	
+	void setLeftEye(Rect& r);
+	Rect& getLeftEye();
+	
+	void setRightEye(Rect& r);
+	Rect& getRightEye();
+	
+	void setMouth(Rect& r);
+	Rect& getMouth();
+	
+	void setNose(Rect& r);
+	Rect& getNose();
+	
+};
+
+#endif /* FACE_H_ */
